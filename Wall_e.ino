3a26f3da84834a8c9d41a431d89cac19eb7368e7
Amr Keleg <amr_mohamed@live.com>
2015-07-12 03:03:01 +0200

    Add first motor code

--- Wall_e.ino
+++ Wall_e.ino
@@ -1,12 +1,57 @@
-void setup()
-{
-  pinMode(13,OUTPUT);
+/************************
+Exercise the motor using
+the L293 chip
+************************/
+
+#define ENABLE 3
+#define DIRB 4
+#define DIRA 5
+
+void setup() {
+  int i;
+
+  //---set pin direction
+  pinMode(ENABLE,OUTPUT);
+  pinMode(DIRA,OUTPUT);
+  pinMode(DIRB,OUTPUT);
+
+  //---back and forth example
+/*
+  digitalWrite(ENABLE,HIGH); // enable on
+  for (i=0;i<5;i++) {
+    digitalWrite(DIRA,HIGH); //one way
+    digitalWrite(DIRB,LOW);
+    delay(500);
+    digitalWrite(DIRA,LOW);  //reverse
+    digitalWrite(DIRB,HIGH);
+    delay(500);
+  }
+  digitalWrite(ENABLE,LOW); // disable
+  delay(4000);
+
+  //---fast/slow stop example
+  digitalWrite(ENABLE,HIGH); //enable on
+  digitalWrite(DIRA,HIGH); //one way
+  digitalWrite(DIRB,LOW);
+  delay(1000);
+  digitalWrite(ENABLE,LOW); //slow stop
+  delay(3000);
+  digitalWrite(ENABLE,HIGH); //enable on
+  digitalWrite(DIRA,HIGH); //one way
+  digitalWrite(DIRB,LOW);
+  delay(1000);
+  digitalWrite(DIRA,LOW); //fast stop
+  delay(3000);
+*/
+  //---PWM example, full speed then slow
+  digitalWrite(ENABLE,HIGH); //enable on
+  digitalWrite(DIRA,HIGH); //one way
+  digitalWrite(DIRB,LOW);
+  delay(2000);
+  analogWrite(ENABLE,128); //half speed
+  delay(10000);
+  digitalWrite(ENABLE,LOW); //all done
 }
-void loop()
-{
-  digitalWrite(13,HIGH);
-  delay(1000);
-  digitalWrite(13,LOW);
-  delay(1000);
-  
+
+void loop() {
 }
